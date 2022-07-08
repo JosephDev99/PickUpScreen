00395f11137604151cbbbe047c6592cff91a3740
Mike Brewer <mike.brewer419@outlook.com>
2022-07-08 16:06:16 +0900

    Delete Google API

--- screens/Map.js
+++ screens/Map.js
@@ -5,32 +5,7 @@ import { View } from 'react-native-web';
 const image = require('../assets/imgpsh_fullsize_anim.png');
 const App = () => {
   const [pickup, setPickup] = useState("");
-  const [pickupLatLong, setPickupLatLong] = useState({});
   const [dropoff, setDropoff] = useState("");
-  const [dropoffLatLong, setDropoffLatLong] = useState({});
-  const [alphabetLetter, setAlphabetLetter] = useState("A");
-  const [tasks, setTasks] = useState([]);
-  const getLatLong = (item, latLong) => {
-    axios
-      .request({
-        method: "post",
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${item}&key=${GEOCODE_API_KEY}`,
-      })
-      .then((response) => {
-        latLong(response.data.results[0].geometry.location);
-      })
-      .catch((e) => {
-        console.log(e.response);
-      });
-  };
-
-  const latLongPickup = (state) => {
-    setPickupLatLong(state);
-  };
-
-  const latLongDropoff = (state) => {
-    setDropoffLatLong(state);
-  };
   return (
     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <View style={styles.container}>
